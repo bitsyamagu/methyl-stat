4789fcbfd2c1128e8ccd954bf030413891f23511
Daisuke Yamaguchi <49506866+bitsyamagu@users.noreply.github.com>
2022-04-20 11:46:54 +0900

    for guppy6

--- methylstat/src/cpp/include/fast5.hpp
+++ methylstat/src/cpp/include/fast5.hpp
@@ -8,15 +8,17 @@ struct modbase {
     char* bases;
     uint32_t  width;
     uint32_t  height;
+    uint32_t  mc_col;
+    uint32_t  ma_col;
 };
 
-  void fast5_open(const char*);
-  const char* read_fastq(const char*);
-  struct modbase  read_modbase(const char*);
-  void free_fastq(char const*);
-  int get_data_index(char const*);
-  void free_modbase( char const*);
-  void fast5_close();
+void fast5_open(const char*);
+const char* read_fastq(const char*);
+struct modbase  read_modbase(const char*);
+void free_fastq(char const*);
+int get_data_index(char const*);
+void free_modbase( char const*);
+void fast5_close();
 
 #ifdef __cplusplus
 }
